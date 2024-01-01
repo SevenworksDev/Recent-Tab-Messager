e62b96359b9f19cca93c06aebb1bbaf9bc57dbe1
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-12-31 19:23:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-import requests
-import base64
-import itertools
-import string
-import threading
-import time
+import requests, base64, itertools, string, threading, time, random
 
-accid = ""  # Account ID, Find it by looking your user up on GDBrowser
-pw = ""  # Your password in plain text
-subject = ""  # Message subject
-body = ""  # Message body
+threads = 25
+accid = "" # get from gdbrowser
+passw = "" # password
+subject = "hi"
+body = "my name is quandale dingle rehehehehehe"
 
-def xor(data, key):
-    return ''.join(chr(ord(x) ^ ord(y)) for (x, y) in zip(data, itertools.cycle(key)))
+def xor(data, key): return ''.join(chr(ord(x) ^ ord(y)) for (x, y) in zip(data, itertools.cycle(key)))
+def base64_encode(string: str) -> str: return base64.urlsafe_b64encode(string.encode()).decode()
+def gjp_encrypt(data): return base64.b64encode(xor(data, "37526").encode()).decode()
+def message_encode(data): return base64_encode(xor(data, '14251'))
 
-def base64_encode(string: str) -> str:
-    return base64.urlsafe_b64encode(string.encode()).decode()
-
-def gjp_encrypt(data):
-    return base64.b64encode(xor(data, "37526").encode()).decode()
-
-def message_encode(data):
-    return base64_encode(xor(data, '14251'))
+def doomsday(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        return random.choice(lines).strip()
 
 def getAccountID():
     userid = requests.post(
@@ -34,24 +28,38 @@ def getAccountID():
         headers={"User-Agent": ""},
     ).text.split(":")[21]
 
-def uploadMessage(id, pw):
-    rr = requests.post(
-        "http://www.boomlings.com/database/uploadGJMessage20.php",
-        data={
-            "accountID": accid,
-            "gjp": gjp_encrypt(pw),
-            "toAccountID": str(getAccountID()),
-            "subject": base64_encode(subject),
-            "body": message_encode(body),
-            "secret": "Wmfd2893gb7",
-        },
-        headers={"User-Agent": ""},
-    ).text
-    if rr == "1":
-        return "Success"
-    else:
-        return "User has messages turned off"
+def uploadMessage(victim):
+    try:
+        banana = doomsday('accounts.txt')
+        proxy = doomsday('proxies.txt').strip()
+
+        rr = requests.post(
+            "http://www.boomlings.com/database/uploadGJMessage20.php",
+            data={
+                "accountID": accid,
+                "gjp": gjp_encrypt(passw),
+                "toAccountID": victim,
+                "subject": base64_encode(subject),
+                "body": message_encode(body),
+                "secret": "Wmfd2893gb7",
+            },
+            headers={"User-Agent": ""},
+            proxies={'http': "http://" + proxy, 'https': "https://" + proxy},
+            timeout=4
+        ).text
+
+        if rr == "1":
+            print(f"Success - Victim: {victim}")
+        else:
+            pass
+    except Exception as e:
+        pass
+
+def lmao(account):
+    for _ in range(threads):
+        threading.Thread(target=uploadMessage, args=(account,)).start()
 
 while True:
-    print(str(uploadMessage(id, pw)))
+    account = getAccountID()
+    lmao(account)
     time.sleep(4)
